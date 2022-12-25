feca94c2322f69c04668bc427bb1de75bdaf9ace
trxsat <120173622+trxsat@users.noreply.github.com>
2022-12-25 21:33:20 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "123456789876:Abkkanbsbaks"
+TOKEN = "5902788732:AAFub-jlegUC16ElcYLHIS9un1habGCP0Hw"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
